c90a6a0d001cb9a726cf000650a39cf9c2e3d97c
Stanislav Levin <stanislav2708@outlook.de>
2024-11-05 16:03:32 +0100

    render summary complete

--- scripts/board.js
+++ scripts/board.js
@@ -6,15 +6,15 @@ let tasks = [
             {
                 id: 1, title: 'Gießen', description: '300ml Wasser gießen',
                 worker: 'Stanislav Levin, Ozan Orhan', due_Date: '31.11.2024',
-                priority: '<img src="../assets/icons/png/prioritySymbolsMiddle.png">',
-                category: 'Technical Task', 
+                priority: '../assets/icons/png/prioritySymbolsMiddle.png',
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 1', 'kleiner schritt 2', 'kleiner schritt 3', 'kleiner schritt 4']
             },
             {
                 id: 2, title: 'coden', description: 'Join coden',
                 worker: 'Stanislav Levin, Kevin Fischer', due_Date: '31.11.2024',
-                priority: '<img src="../assets/icons/png/prioritySymbolsLow.png">',
-                category: 'Technical Task', 
+                priority: '../assets/icons/png/prioritySymbolsLow.png',
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 5', 'kleiner schritt 6', 'kleiner schritt 7', 'kleiner schritt 8']
             }
         ]
@@ -27,14 +27,14 @@ let tasks = [
                 id: 3, title: 'kochen', description: 'braten machen',
                 worker: 'Stanislav Levin, Nicolai Österle', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsUrgent.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 9', 'kleiner schritt 10', 'kleiner schritt 11', 'kleiner schritt 12']
             },
             {
                 id: 4, title: 'essen', description: 'genug nährstoffe aufnehmen',
                 worker: 'Stanislav Levin, Ozan Orhan', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsMiddle.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 13', 'kleiner schritt 14', 'kleiner schritt 15', 'kleiner schritt 16']
             }]
     },
@@ -46,14 +46,14 @@ let tasks = [
                 id: 5, title: 'trainieren', description: 'physis und psyche auslasten',
                 worker: 'Stanislav Levin, Kevin Fischer', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsMiddle.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 17', 'kleiner schritt 18', 'kleiner schritt 19', 'kleiner schritt 20']
             },
             {
                 id: 6, title: 'einkaufen', description: 'kein Müll als nahrung',
                 worker: 'Stanislav Levin, Nicolai Österle', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsLow.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 21', 'kleiner schritt 22', 'kleiner schritt 23', 'kleiner schritt 24']
             }]
     },
@@ -65,14 +65,14 @@ let tasks = [
                 id: 7, title: 'schlafen', description: 'ausreichend schlafen',
                 worker: 'Stanislav Levin, Ozan Orhan', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsUrgent.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 25', 'kleiner schritt 26', 'kleiner schritt 27', 'kleiner schritt 28']
             },
             {
                 id: 8, title: 'backen', description: 'Infused Brownies backen',
                 worker: 'Stanislav Levin, Kevin Fischer', due_Date: '31.11.2024',
                 priority: '../assets/icons/png/prioritySymbolsMiddle.png',
-                category: 'Technical Task', 
+                category: 'Technical Task',
                 subtasks: ['kleiner schritt 29', 'kleiner schritt 30', 'kleiner schritt 31', 'kleiner schritt 32']
             }]
     }
@@ -82,9 +82,15 @@ let tasks = [
 function renderSummary() {
     tasks.forEach(list => {
         const content = document.getElementById(`${list.id}List`);
-        if (!content) return;
-        list.task.forEach(task => {
+        if (list.task.length === 0) {
             content.innerHTML += /*html*/`
+                <div class="nothingToDo">
+                    <p class="margin_0">No Tasks To-do</p>
+                </div>
+            `;
+        } else {
+            list.task.forEach(task => {
+                content.innerHTML += /*html*/`
                 <div class="boardCard">
                     <p style="background-color: blue;" class="taskCategory">${task.category}</p>
                     <p style="font-weight: 700;">${task.title}</p>
@@ -102,7 +108,8 @@ function renderSummary() {
                         <img class="priority" src="${task.priority}"> <!-- Hier wird der Pfad verwendet -->
                     </div>
                 </div>
-            `;
-        });
+                `;
+            });
+        }
     });
 }
