3f38b9892e2acaebfd47f69aaa48ecb2abb0b2a1
Stanislav Levin <stanislav2708@outlook.de>
2024-11-07 16:54:34 +0100

    finish board HTML CSS JS

--- scripts/board.js
+++ scripts/board.js
@@ -12,7 +12,7 @@ let tasks = [
                     { name: 'Ozan Orhan', class: 'worker-ozan' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsMiddle.png',
+                priority: 'Middle',
                 category: { name: 'Technical Task', class: 'category-technical' },
                 subtasks: ['Wasser abstehen lassen', 'Dünger hinzugeben', 'PH Wert anpassen', 'im Ring gießen']
             },
@@ -25,7 +25,7 @@ let tasks = [
                     { name: 'Kevin Fischer', class: 'worker-kevin' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsLow.png',
+                priority: 'Low',
                 category: { name: 'Technical Task', class: 'category-technical' },
                 subtasks: ['JS Datei einbinden', 'Summary Styling bearbeiten', 'auf github pushen', 'mit Team besprechen']
             }
@@ -37,29 +37,29 @@ let tasks = [
         task: [
             {
                 id: 3,
-                title: 'kochen',
-                description: 'braten machen',
+                title: 'HTML templates einbinden',
+                description: 'alle HTML datein zusammenfassen',
                 workers: [
                     { name: 'Stanislav Levin', class: 'worker-stanislav' },
                     { name: 'Nicolai Österle', class: 'worker-nicolai' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsUrgent.png',
+                priority: 'Urgent',
                 category: { name: 'Technical Task', class: 'category-technical' },
-                subtasks: ['kleiner schritt 9', 'kleiner schritt 10', 'kleiner schritt 11', 'kleiner schritt 12']
+                subtasks: ['Code Schnipsel sammeln', 'auf github mergen', 'mit dem Team besprechen', 'änderungen anpassen und clean code beachten']
             },
             {
                 id: 4,
-                title: 'essen',
-                description: 'genug nährstoffe aufnehmen',
+                title: 'add Task einbinden',
+                description: 'add task erfolgreich ins array einbinden',
                 workers: [
                     { name: 'Stanislav Levin', class: 'worker-stanislav' },
                     { name: 'Ozan Orhan', class: 'worker-ozan' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsMiddle.png',
+                priority: 'Middle',
                 category: { name: 'User Story', class: 'category-user-story' },
-                subtasks: ['kleiner schritt 13', 'kleiner schritt 14', 'kleiner schritt 15', 'kleiner schritt 16']
+                subtasks: ['datenstruktur besprechen', 'Änderungen übernehmen']
             }
         ]
     },
@@ -69,29 +69,29 @@ let tasks = [
         task: [
             {
                 id: 5,
-                title: 'trainieren',
-                description: 'physis und psyche auslasten',
+                title: 'contacts einbinden',
+                description: 'codeblöcke miteinander verbinden',
                 workers: [
                     { name: 'Stanislav Levin', class: 'worker-stanislav' },
                     { name: 'Kevin Fischer', class: 'worker-kevin' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsMiddle.png',
+                priority: 'Middle',
                 category: { name: 'Technical Task', class: 'category-technical' },
-                subtasks: ['kleiner schritt 17', 'kleiner schritt 18', 'kleiner schritt 19', 'kleiner schritt 20']
+                subtasks: ['contacts array erstellen', 'die daten im tasks aktualisieren', 'dateien einbinden', 'Contacts anzeigen lassen']
             },
             {
                 id: 6,
-                title: 'einkaufen',
-                description: 'kein Müll als Nahrung',
+                title: 'bootstrap einbinden',
+                description: 'sauberes einbinden der ordner',
                 workers: [
                     { name: 'Stanislav Levin', class: 'worker-stanislav' },
                     { name: 'Nicolai Österle', class: 'worker-nicolai' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsLow.png',
+                priority: 'Low',
                 category: { name: 'Technical Task', class: 'category-technical' },
-                subtasks: ['kleiner schritt 21', 'kleiner schritt 22', 'kleiner schritt 23', 'kleiner schritt 24']
+                subtasks: ['cloud einstellungen anpassen', 'neues initialisieren', 'login und registration mit login fertig bauen', 'login als index deklarieren']
             }
         ]
     },
@@ -108,7 +108,7 @@ let tasks = [
                     { name: 'Ozan Orhan', class: 'worker-ozan' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsUrgent.png',
+                priority: 'Urgent',
                 category: { name: 'Technical Task', class: 'category-technical' },
                 subtasks: ['kleiner schritt 25', 'kleiner schritt 26', 'kleiner schritt 27', 'kleiner schritt 28']
             },
@@ -121,7 +121,7 @@ let tasks = [
                     { name: 'Kevin Fischer', class: 'worker-kevin' }
                 ],
                 due_Date: '31.11.2024',
-                priority: '../assets/icons/png/prioritySymbolsMiddle.png',
+                priority: 'Middle',
                 category: { name: 'User Story', class: 'category-user-story' },
                 subtasks: ['kleiner schritt 29', 'kleiner schritt 30', 'kleiner schritt 31', 'kleiner schritt 32']
             }
@@ -131,6 +131,7 @@ let tasks = [
 
 
 let currentDraggedElement;
+
 
 function renderBoard() {
     tasks.forEach(list => {
@@ -162,7 +163,7 @@ function renderBoard() {
                                 `<p class="${worker.class} workerEmblem" style="margin-left: ${index === 1 ? '-10px' : '0'};">${worker.name.charAt(0)}</p>`
                             ).join('')}
                         </div>
-                        <img class="priority" src="${task.priority}"> 
+                        <img class="priority" src="../../assets/icons/png/PrioritySymbols${task.priority}.png"> 
                     </div>
                 </div>
             `;
@@ -172,18 +173,18 @@ function renderBoard() {
 }
 
 
-
 function startDragging(taskId) {
     currentDraggedElement = taskId;
 }
+
 
 function allowDrop(event) {
     event.preventDefault();
 }
 
+
 function handleDrop(event, targetListId) {
-    event.preventDefault();
-    
+    event.preventDefault();  
     let sourceList, task;
     tasks.forEach(list => {
         const taskIndex = list.task.findIndex(t => t.id === currentDraggedElement);
@@ -191,14 +192,56 @@ function handleDrop(event, targetListId) {
             sourceList = list;
             [task] = sourceList.task.splice(taskIndex, 1); 
         }
-    });
-
-    
+    }); 
     const targetList = tasks.find(list => list.id === targetListId);
     if (targetList && task) {
         targetList.task.push(task); 
     }
+    renderBoard();
+}
 
-    
-    renderSummary();
+
+function findTask() {
+    const searchTerm = document.getElementById('findTask').value.toLowerCase();
+
+    tasks.forEach(list => {
+        const content = document.getElementById(`${list.id}List`).querySelector('.taskContainer');
+        content.innerHTML = ""; 
+        const filteredTasks = list.task.filter(task =>
+            task.title.toLowerCase().includes(searchTerm) ||
+            task.description.toLowerCase().includes(searchTerm)
+        );
+
+        if (filteredTasks.length === 0) {
+            content.innerHTML = /*html*/`
+                <div class="nothingToDo">
+                    <p class="margin_0">No matching Task found</p>
+                </div>
+            `;
+        } else {
+            filteredTasks.forEach(task => {
+                content.innerHTML += /*html*/`
+                    <div draggable="true" ondragstart="startDragging(${task.id})" class="boardCard">
+                        <p class="${task.category.class} taskCategory">${task.category.name}</p>
+                        <p style="font-weight: 700;">${task.title}</p>
+                        <p style="color: #A8A8A8;">${task.description}</p>
+                        <div class="d_flex">
+                            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar" style="width: 50%"></div>
+                            </div>
+                            <p style="padding-left: 15px; font-size: 12px;">${task.subtasks.length} Subtasks</p>
+                        </div>
+                        <div class="d_spaceBetween">
+                            <div class="worker">
+                                ${task.workers.map((worker, index) =>
+                                    `<p class="${worker.class} workerEmblem" style="margin-left: ${index === 1 ? '-10px' : '0'};">${worker.name.charAt(0)}</p>`
+                                ).join('')}
+                            </div>
+                            <img class="priority" src="../../assets/icons/png/PrioritySymbols${task.priority}.png"> 
+                        </div>
+                    </div>
+                `;
+            });
+        }
+    });
 }
