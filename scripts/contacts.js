ead5377d482641002d5a53e705eb5aa1eafca087
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2024-11-09 13:31:55 +0100

    changed loadContacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,72 +1,20 @@
-let contacts = [
-    {
-        "userID": 1,
-        "firstName": "Albert",
-        "lastName": "Neu",
-        "email": "aneu@gmail.com",
-        "phone": "+491234347"
+let contacts = [];
 
-    },
-    {
-        "userID": 2,
-        "firstName": "Heinz",
-        "lastName": "Alt",
-        "email": "halt@gmail.com",
-        "phone": "+491234853"
 
-    },
-    {
-        "userID": 3,
-        "firstName": "Felix",
-        "lastName": "Test",
-        "email": "ftest@gmail.com",
-        "phone": "+491237531"
-
-    },
-    {
-        "userID": 4,
-        "firstName": "Kevin",
-        "lastName": "Fischer",
-        "email": "kfischer@gmail.com",
-        "phone": "+491234976"
-
-    },
-    {
-        "userID": 4,
-        "firstName": "Kevin",
-        "lastName": "Fischer",
-        "email": "kfischer@gmail.com",
-        "phone": "+491234419"
-
-    },
-    {
-        "userID": 5,
-        "firstName": "Nicolai",
-        "lastName": "Österle",
-        "email": "nösterle@gmail.com",
-        "phone": "+491234567"
-
-    },
-    {
-        "userID": 6,
-        "firstName": "Stanislav",
-        "lastName": "Levin",
-        "email": "slevin@gmail.com",
-        "phone": "+491234357"
-
-    },
-
-];
-
+const baseUrl = "https://test-b993b-default-rtdb.europe-west1.firebasedatabase.app/";
 
 function renderContacts() {
-    contacts.sort((a, b) => a.firstName.localeCompare(b.firstName));
+ 
+
+    
+
+     contacts.sort((a, b) => a.firstName.localeCompare(b.firstName));
 
     document.getElementById('contacts').innerHTML = "";
     let currentLetter = ""; 
 
-    for (let i = 0; i < contacts.length; i++) {
-        let firstLetter = contacts[i].firstName.slice(0, 1);
+    for (let i = 1; i < contacts.length; i++) {
+        let firstLetter = contacts.i.firstName.slice(0, 1);
 
         if (firstLetter !== currentLetter) {
             currentLetter = firstLetter;
@@ -87,4 +35,26 @@ function renderContactDetails(i){
 }
 
 
+async function loadContacts(){
 
+   let response= await fetch(baseUrl + ".json")
+   let contactData = await response.json();
+   let contactDatas =[];
+   contacts.push(contactData);
+   
+for (let i = 1; i < contactDatas.length; i++) {
+    let UserInfos = {
+        id: i,
+        firstName: contacs[0].contacts[i].firstName,
+        lastName: contacts[0].contacts[i].lastName,
+        email: contacts[0].contacts[i].email,
+        phone: contacts[0].contacts[i].phone
+    };
+    contacts.push(UserInfos);
+}
+console.log(contacts);
+
+renderContacts();
+
+
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -11,11 +11,11 @@ function contactTemps(i) {
     return `<div class="contact">
                 <div class="shortInfo" onclick="renderContactDetails(${i})">
                     <div class="tag">
-                        <p class="ContactUserTag">${contacts[i].firstName.slice(0, 1)}${contacts[i].lastName.slice(0, 1)}</p>
+                        <p class="ContactUserTag">${contacts[0].contacts[i].firstName.slice(0, 1)}${contacts[0].contacts[i].lastName.slice(0, 1)}</p>
                     </div>
                     <div>
-                        <p>${contacts[i].firstName} ${contacts[i].lastName}</p>
-                        <a href="mailto:${contacts[i].email}"><p>${contacts[i].email}</p></a>
+                        <p>${contacts[0].contacts[i].firstName} ${contacts[0].contacts[i].lastName}</p>
+                        <a href="mailto:${contacts[0].contacts[i].email}"><p>${contacts[0].contacts[i].email}</p></a>
                     </div>
                 </div>
             </div>`;
@@ -24,15 +24,15 @@ function contactTemps(i) {
 
 function ContactDetailsTemps(i) {
     return `<div class="InfoBoxHead">
-    <div><p class="userTagBig">${contacts[i].firstName.slice(0, 1)}${contacts[i].lastName.slice(0, 1)}</p></div>
+    <div><p class="userTagBig">${contacts[0].contacts[i].firstName.slice(0, 1)}${contacts[0].contacts[i].lastName.slice(0, 1)}</p></div>
     <div>
-        <div><H2>${contacts[i].firstName} ${contacts[i].lastName}</H2></div>
+        <div><H2>${contacts[0].contacts[i].firstName} ${contacts[0].contacts[i].lastName}</H2></div>
         <div><a href=""><img src="../assets/icons/png/edit.png" alt=""></a><a href=""><img src="../assets/icons/png/Delete contact.png" alt=""></a></div>
     </div>
     </div>
     <h4>Contact Information</h4>
     <div>
-        <div><b>Email</b><br><a href="mailto:"${contacts[i].email}">${contacts[i].email}</a></div>
-        <div><b>Phone</b><br>${contacts[i].phone}</div>
+        <div><b>Email</b><br><a href="mailto:"${contacts[0].contacts[i].email}">${contacts[0].contacts[i].email}</a></div>
+        <div><b>Phone</b><br>${contacts[0].contacts[i].phone}</div>
     </div> `;
 }
